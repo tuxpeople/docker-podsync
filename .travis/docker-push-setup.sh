2f3f730497a41007c46bf2734d018e76f0064e35
Thomas Deutsch <thomas@tuxpeople.org>
2020-10-31 11:02:43 +0100

    fixing githubs new default

--- .travis/docker-push-setup.sh
+++ .travis/docker-push-setup.sh
@@ -3,7 +3,7 @@
 #https://graysonkoonce.com/getting-the-current-branch-name-during-a-pull-request-in-travis-ci
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
-if [ "$BRANCH" == "master" ]; then
+if [ "$BRANCH" == "master" ] || [ "$BRANCH" == "main" ]; then
     echo "This commit has been merged to master so on success images will be pushed"
     export SHOULD_DOCKER_PUSH=true
     export TAGS=latest
